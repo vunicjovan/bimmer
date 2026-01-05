8462b21887b3dc890a178a25de72ede032892dd8
Jovan Vunic <jovanvunicnovisad@gmail.com>
2026-01-05 23:15:39 +0100

    Remove the redundant file

--- models.py
+++
@@ -1,9 +0,0 @@
-from typing import Optional
-
-# Line 31 fix - use Optional[str] and forward reference
-def __init__(self, ..., field_name: Optional[str] = None, user: Optional["User"] = None):
-    # ...existing code...
-
-# Lines 51 and 53 fix - use Optional[str] and forward reference
-def __init__(self, ..., field_name: Optional[str] = None, user: Optional["User"] = None):
-    # ...existing code...
